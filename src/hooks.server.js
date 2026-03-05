8836ce465354459f39148622b3a4365df4fbd224
Taylor Liesnham <tliesnham@icloud.com>
2026-03-05 19:30:26 +0000

    add hsts status

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -31,6 +31,7 @@ export async function handle({ event, resolve }) {
         }
     })
 
+    response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
     response.headers.set('X-Frame-Options', 'DENY')
     response.headers.set('X-Content-Type-Options', 'nosniff')
     response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
