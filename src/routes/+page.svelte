0ce082e7c55788b440f21415d67de97b1e135751
Taylor Liesnham <tliesnham@icloud.com>
2026-04-06 12:06:52 +0100

    amend background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -178,7 +178,7 @@
     </script>
 </svelte:head>
 
-<div class="min-h-screen bg-[#FDFBF7] selection:bg-[#FFF0F3] selection:text-pink-900 flex flex-col relative">
+<div class="min-h-screen selection:bg-[#FFF0F3] selection:text-pink-900 flex flex-col relative">
     
     <Navigation />
     
